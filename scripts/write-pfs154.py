585999b40d324f3aa276a765e6f3b3d500568bc3
Marcos Del Sol Vives <marcos@orca.pet>
2019-09-02 01:15:19 +0200

    Rewrite write-pfs154 to allow uploading of Intel HEX files

--- scripts/write-pfs154.py
+++ scripts/write-pfs154.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python3
 
-from progproto import *
 from array import array
-import time
+from intelhex import IntelHex16bit
+from progproto import *
+import argparse
 
-data = array('H', [0] * 8)
+parser = argparse.ArgumentParser(description='Writes hex images to Padauk PFS154 chips')
+parser.add_argument('--erase', action='store_true', help='Forces a chip erase. If no specified, only if the flash is occupied currently it will be formatted')
+parser.add_argument('--no-verify', action='store_true', help='Skips verification')
+parser.add_argument('hex', type=argparse.FileType('r'), help='hex file')
+parser.add_argument('com', help='serial port')
+args = parser.parse_args()
 
+ih = IntelHex16bit(args.hex)
 
-with Programmer('COM3') as p:
+with Programmer(args.com) as p:
 	try:
 		p.start_write()
-		for i in range(0, 48, 8):
-			print(i)
-			p.write(i, data)
-			#time.sleep(0.01)
+		for start, size in ih.segments():
+			print('Writing %04X-%04X... ' % (start, start + size - 1), end='')
+			wordarray = ih.tobinarray(start=start, size=size)
+			p.write(start, wordarray)
+			print('done')
+		if not args.no_verify:
+			p.start_read()
+			for start, size in ih.segments():
+				print('Verifying %04X-%04X... ' % (start, start + size - 1), end='')
+				expected = ih.tobinarray(start=start, size=size)
+				read = p.read(start, size)
+				failed = False
+				for i in range(0, len(expected)):
+					if (expected[i] and 0x3FFF) != (read[i] and 0x3FFF):
+						if not failed:
+							print('FAILED')
+							failed = True
+						print('@ %04X - expected %04X, read %04X' % (start + i, expected[i], read[i]))
+				if not failed:
+					print('OK')
+			
 	finally:
 		p.finish()
